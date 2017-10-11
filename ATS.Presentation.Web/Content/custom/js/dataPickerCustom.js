85c02e4d66206ff08cff0faecaa68b2a637df5df
Alan Bessa <bessa.alan@gmail.com>
2017-10-11 08:29:28 -0300

    v2

--- ATS.Presentation.Web/Content/custom/js/dataPickerCustom.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function ()
-{    
-    //Date picker
-    $('.datepicker').datepicker({
-        autoclose: true,
-        language: "pt-BR",
-        format: "dd/mm/yyyy"
-    });
-});
